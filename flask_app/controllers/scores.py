5d36f3e80dc5724bb507606bc4833c932e8a16df
Laura Rasband <107084227+lmrasband90@users.noreply.github.com>
2022-12-21 14:43:40 -0500

    Create scores.py

---
+++ flask_app/controllers/scores.py
@@ -0,0 +1,5 @@
+from flask_app import app
+from flask import redirect, render_template, request, session, flash
+from flask_app.models.score import Score
+from flask_app.models.user import User
+# from flask_app.models.word import Word
